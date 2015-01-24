28ad579dcd1d1010fb7c9413a9729dde73d67078
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2015-01-24 13:39:17 +0100

    Initial Commit

---
+++ Cimmerian Purple/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cimmerian Purple
+//
+//  Created by Nevyn Bengtsson on 2015-01-24.
+//  Copyright (c) 2015 Thirdcog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
